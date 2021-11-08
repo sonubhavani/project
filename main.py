4aa38cf34f804e0191114a4c63e43db05e524876
bhavani <nageshwar@LAPTOP-S4NFEDAE>
2021-11-08 20:15:04 +0530

    sonu

---
+++ main.py
@@ -0,0 +1,6 @@
+mystr=int(int("enter a string")
+if str(mystr)==mystr[::-1]
+	print("it is palindrome")
+else:
+	print(" it is not a palindrome")
+ 
---
+++ square.py
@@ -0,0 +1,5 @@
+n=int("enter a number")
+if(number**0.5)%1==0:
+	print(number,"it is a perfect square")
+else:
+	print(number,"it is not a square")
