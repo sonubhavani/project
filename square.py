5aa1e0290f902f589396e89a486fc76a10d8b289
bhavani <nageshwar@LAPTOP-S4NFEDAE>
2021-11-10 10:31:53 +0530

    print statement was updated

--- square.py
+++ square.py
@@ -1,5 +1,5 @@
 n=int("enter a number")
 if(number**0.5)%1==0:
-	print(number,"it is a perfect square")
+	print(number,"given  number is a perfect square")
 else:
 	print(number,"it is not a square")
